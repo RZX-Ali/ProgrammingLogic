0655b8cdaff230f514ee17204073e0fcdafbda4d
RZX-Ali <85710786+RZX-Ali@users.noreply.github.com>
2025-08-03 22:09:23 -0400

    Add files via upload

---
+++ Lab5/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab5;
+
+
+// Step 1
+class Animal
+{
+    public virtual void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+
+// Step 2
+class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat says: meow");
+    }
+}
+
+class Dog : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+
+        // Step 3
+        Animal baseAnimal = new Animal();
+        Animal myCat = new Cat();
+        Animal myDog = new Dog();
+
+        baseAnimal.animalSound();
+        myCat.animalSound();
+        myDog.animalSound();    
+    }
+}
